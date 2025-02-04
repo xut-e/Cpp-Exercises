f6c529316e3e444ce0bb288471cb4f752110855b
xut <xut.workmail@gmail.com>
2025-02-05 00:45:44 +0100

    Update Exercise6_09.cpp

--- Topic_6/Exercise6_09.cpp
+++ Topic_6/Exercise6_09.cpp
@@ -6,22 +6,97 @@ identifique cuál de ambas versiones está más actualizada.*/
 #include <string>
 
 int main() {
-    std::string version1, version2;
-    int version_v1, version_v2, subversion_v1, subversion_v2, parche_v1, parche_v2;
+    std::string v1, v2;
+    int contadorNumeros = 0;
+    int version_v1 = 0, subversion_v1 = 0, parche_v1 = 0;
+    int version_v2 = 0, subversion_v2 = 0, parche_v2 = 0;
 
     std::cout << "Introduce una version ([version].[subversion].[parche] Ej: 1.4.5): ";
-    std::getline(std::cin, version1);
-
+    std::getline(std::cin, v1);
     std::cout << "Introduce otra version: ";
-    std::getline(std::cin, version2);
+    std::getline(std::cin, v2);
 
-
-    for (int i = 0; i < version1.length(); i++)
-    {
-        if ()
+    contadorNumeros = 0;
+    for (int i = 0; i < v1.length(); i++) {
+        if (v1[i] == '.')
+        {
+            break;
+        }
+        version_v1 = version_v1 * 10 + (v1[i] - '0');
+        contadorNumeros++;
     }
 
+    contadorNumeros++;
+    subversion_v1 = 0;
+    for (int i = contadorNumeros; i < v1.length(); i++) {
+        if (v1[i] == '.')
+        {
+            break;
+        }
+        subversion_v1 = subversion_v1 * 10 + (v1[i] - '0');
+        contadorNumeros++;
+    }
 
+    contadorNumeros++;
+    parche_v1 = 0;
+    for (int i = contadorNumeros; i < v1.length(); i++) {
+        parche_v1 = parche_v1 * 10 + (v1[i] - '0');
+    }
+
+    contadorNumeros = 0;
+    for (int i = 0; i < v2.length(); i++) {
+        if (v2[i] == '.')
+        {
+            break;
+        }
+        
+        version_v2 = version_v2 * 10 + (v2[i] - '0');
+        contadorNumeros++;
+    }
+
+    contadorNumeros++;
+    subversion_v2 = 0;
+    for (int i = contadorNumeros; i < v2.length(); i++) {
+        if (v2[i] == '.')
+        {
+            break;
+        }
+            
+        subversion_v2 = subversion_v2 * 10 + (v2[i] - '0');
+        contadorNumeros++;
+    }
+
+    contadorNumeros++;
+    parche_v2 = 0;
+    for (int i = contadorNumeros; i < v2.length(); i++) {
+        parche_v2 = parche_v2 * 10 + (v2[i] - '0');
+    }
+
+    if (version_v1 > version_v2) {
+        std::cout << "La version 1 es la mas reciente." << std::endl;
+    }
+    else if (version_v1 < version_v2) {
+        std::cout << "La version 2 es la mas reciente." << std::endl;
+    }
+    else {
+        if (subversion_v1 > subversion_v2) {
+            std::cout << "La version 1 es la mas reciente." << std::endl;
+        }
+        else if (subversion_v1 < subversion_v2) {
+            std::cout << "La version 2 es la mas reciente." << std::endl;
+        }
+        else {
+            if (parche_v1 > parche_v2) {
+                std::cout << "La version 1 es la mas reciente." << std::endl;
+            }
+            else if (parche_v1 < parche_v2) {
+                std::cout << "La version 2 es la mas reciente." << std::endl;
+            }
+            else {
+                std::cout << "Son la misma version." << std::endl;
+            }
+        }
+    }
 
     return 0;
 }
