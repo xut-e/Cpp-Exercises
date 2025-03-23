98f1dc3a5acb01bd3467714511374cdb9cb0c753
xut <xut.workmail@gmail.com>
2025-03-23 22:24:30 +0100

    Update Exercise8_04.cpp

--- Topic_8/Exercise8_04.cpp
+++ Topic_8/Exercise8_04.cpp
@@ -10,87 +10,122 @@ El programa selecciona aleatoriamente una palabra de una lista predefinida.
  #include <string>
  #include <time.h>
  
- 
  std::vector<std::string> inicializarDiccionario() {
-     return {"gato", "perro", "casa", "árbol", "sol", "luna", "montaña", "río", "coche", "avión", "computadora", "libro", "silla", "mesa", "ventana", "puerta", "jardín", "flor", "pelota", "camisa", "zapato", "reloj", "botella", "lámpara", "teléfono", "guitarra", "piano", "televisión", "espejo", "cuchara", "tenedor", "cuchillo", "plato", "vaso", "sombrero", "bufanda", "abrigo", "pantalón", "falda", "vestido", "calcetín", "bolsillo", "maleta", "billete", "moneda", "llave", "martillo", "tornillo", "clavo", "destornillador"};
+     return { "gato", "perro", "casa", "árbol", "sol", "luna", "montaña", "río", "coche", "avión", "computadora", "libro", "silla", "mesa", "ventana", "puerta", "jardín", "flor", "pelota", "camisa", "zapato", "reloj", "botella", "lámpara", "teléfono", "guitarra", "piano", "televisión", "espejo", "cuchara", "tenedor", "cuchillo", "plato", "vaso", "sombrero", "bufanda", "abrigo", "pantalón", "falda", "vestido", "calcetín", "bolsillo", "maleta", "billete", "moneda", "llave", "martillo", "tornillo", "clavo", "destornillador" };
  }
  
  std::string seleccionarPalabra(std::vector<std::string> listaPalabras) {
-     int numeroAleatorio = rand() % (listaPalabras.size() + 1);
- 
-         std::string palabraSeleccionada = listaPalabras[numeroAleatorio];
- 
-         return palabraSeleccionada;
+     int numeroAleatorio = rand() % listaPalabras.size();
+     std::string palabraSeleccionada = listaPalabras[numeroAleatorio];
+     return palabraSeleccionada;
  }
  
  std::vector<char> inicializarFormaPalabra(std::string palabraAAdivinar) {
      std::vector<char> formaPalabraAAdivinar;
  
-     for (size_t i = 0; i < palabraAAdivinar.size(); i++)
-     {
+     for (size_t i = 0; i < palabraAAdivinar.size(); i++) {
          formaPalabraAAdivinar.push_back('_');
      }
  
      return formaPalabraAAdivinar;
  }
  
- bool palabraAdivinada() {
-     
-     
+ bool palabraAdivinada(std::string palabraAAdivinar, std::vector<char>& palabraMostrarUsuario) {
+     std::string palabramostrarusuario;
+ 
+     for (size_t i = 0; i < palabraMostrarUsuario.size(); i++) {
+         palabramostrarusuario += palabraMostrarUsuario[i];
+     }
+ 
+     return palabraAAdivinar == palabramostrarusuario;
  }
  
  bool letraIntentada(std::vector<char> listaLetrasIntentadas, char letraIntroducida) {
-     for (size_t i = 0; i < listaLetrasIntentadas.size(); i++)
-     {
-         if (letraIntroducida == listaLetrasIntentadas[i])
-         {
-             return true;
-             break;
+     for (size_t i = 0; i < listaLetrasIntentadas.size(); i++) {
+         if (letraIntroducida == listaLetrasIntentadas[i]) {
+             return false;
          }
      }
+     return true;
+ }
  
-     return false;
+ std::vector<char> comprobarLetra(char letraIntroducida, std::vector<char>& palabraMostrarUsuario, std::string palabraAAdivinar, std::vector<char>& letrasIntentadas) {
+     for (size_t i = 0; i < palabraAAdivinar.size(); i++) {
+         if (letraIntroducida == palabraAAdivinar[i]) {
+             palabraMostrarUsuario[i] = letraIntroducida;
+         }
+     }
+     letrasIntentadas.push_back(letraIntroducida);
+     return palabraMostrarUsuario;
  }
  
  void jugar(std::string palabraAAdivinar) {
-     int intentos = 0, turno = 1;
+     int intentos = 0, turno = 1, pistas = 1;
      std::vector<char> palabraMostrarAlUsuario = inicializarFormaPalabra(palabraAAdivinar), letrasIntentadas = {};
      char letraIntroducida;
-     
-     while (intentos < palabraAAdivinar.size() && !palabraAdivinada())
-     {
+ 
+     while (intentos < palabraAAdivinar.size() && !palabraAdivinada(palabraAAdivinar, palabraMostrarAlUsuario)) {
          std::cout << "Turno: " << turno << std::endl;
          std::cout << "Palabra: ";
  
-         for (size_t i = 0; i < palabraMostrarAlUsuario.size(); i++)
-         {
+         for (size_t i = 0; i < palabraMostrarAlUsuario.size(); i++) {
              std::cout << palabraMostrarAlUsuario[i] << " ";
          }
          std::cout << std::endl;
  
          std::cout << "Letras intentadas: ";
  
-         for (size_t i = 0; i < letrasIntentadas.size(); i++)
-         {
+         for (size_t i = 0; i < letrasIntentadas.size(); i++) {
              std::cout << letrasIntentadas[i];
  
-             if (i < letrasIntentadas.size() - 1)
-             {
+             if (i < letrasIntentadas.size() - 1) {
                  std::cout << ", ";
              }
          }
          std::cout << std::endl;
  
-         std::cout << "Intentos restantes: " << palabraMostrarAlUsuario.size() - intentos << std::endl;
+         std::cout << "Intentos restantes: " << palabraAAdivinar.size() - intentos << std::endl;
  
          std::cout << "Introduce una letra: ";
  
-         do
-         {
+         do {
              std::cin >> letraIntroducida;
          } while (!letraIntentada(letrasIntentadas, letraIntroducida));
  
+         bool letraEnPalabra = false;
+         for (size_t i = 0; i < palabraAAdivinar.size(); i++) {
+             if (letraIntroducida == palabraAAdivinar[i]) {
+                 letraEnPalabra = true;
+                 break;
+             }
+         }
  
+         if (!letraEnPalabra) {
+             intentos++;
+         }
+ 
+         palabraMostrarAlUsuario = comprobarLetra(letraIntroducida, palabraMostrarAlUsuario, palabraAAdivinar, letrasIntentadas);
+ 
+         if (intentos == palabraAAdivinar.size() - 2 && pistas > 0) {
+             for (size_t i = 0; i < palabraAAdivinar.size(); i++) {
+                 if (palabraMostrarAlUsuario[i] == '_') {
+                     palabraMostrarAlUsuario[i] = palabraAAdivinar[i];
+                     break;
+                 }
+             }
+             pistas--;
+         }
+ 
+         turno++;
+ 
+         system("cls");
+     }
+ 
+     if (palabraAdivinada(palabraAAdivinar, palabraMostrarAlUsuario)) {
+         std::cout << "Enhorabuena, has adivinado la palabra! La palabra era: " << palabraAAdivinar << std::endl;
+     }
+     else {
+         std::cout << "Vaya, no has adivinado la palabra! La palabra era: " << palabraAAdivinar << std::endl;
      }
  }
  
